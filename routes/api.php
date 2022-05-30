62b693cdfd92b5d889550ccecb08e91e7f505efb
rider <myintthwaykhine127@gmail.com>
2022-05-30 10:29:40 +0630

    item todo api setup complete

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ItemController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(ItemController::class)->group(function () {
+    Route::get('/items', 'index');
+    Route::prefix('/item')->group(function () {
+        Route::post('/store', 'store');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'destroy');
+    });
+});
